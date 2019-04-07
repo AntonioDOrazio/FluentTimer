2d089b0379d6a11bcb284e2b7afffa73fe4c083b
Antonio <antonio.dorazio9@outlook.it>
2019-04-07 17:17:39 +0200

    gitignore fix

---
+++ TomatoTimer/TomatoTimer/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
+namespace TomatoTimer
+{
+    /// <summary>
+    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// </summary>
+    /// 
+
+
+    public class AnotherPagePayload
+    {
+        public int TotCicli { get; set; }
+        public double DurataCiclo { get; set; }
+        public double DurataPausa { get; set; }
+    }
+
+
+    public sealed partial class MainPage : Page
+    {
+        List<Int32> Numbers = new List<Int32>();
+
+        public static int NumberChoice { get; set; } = 0;
+        public  TimeSpan? CicleLength { get => cicleLength; set => cicleLength = value; }
+        public  TimeSpan? PauseLength { get => pauseLength; set => pauseLength = value; }
+
+        private static TimeSpan? pauseLength = null;
+        private static TimeSpan? cicleLength = null;
+        public MainPage()
+        {
+
+            this.InitializeComponent();
+
+            for (Int32 i=1; i<7; i++)
+            {
+                Numbers.Add(i);
+            }
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (NumberChoice != 0 && pauseLength != null && cicleLength != null)
+            {
+                AnotherPagePayload payload = new AnotherPagePayload();
+                payload.TotCicli = NumberChoice;
+                TimeSpan NonNullableTs;
+                NonNullableTs = (TimeSpan) CicleLength;
+                payload.DurataCiclo = NonNullableTs.TotalSeconds;
+                NonNullableTs = (TimeSpan)PauseLength;
+                payload.DurataPausa = NonNullableTs.TotalSeconds;
+                this.Frame.Navigate(typeof(Timer), payload);
+
+            }
+            else
+            {
+                await emptyFieldsDialog.ShowAsync();
+            }
+        }
+
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            NumberChoice = (int) MyListBox.SelectedItem;
+        }
+
+        private void CicleTime_SelectedTimeChanged(TimePicker sender, TimePickerSelectedValueChangedEventArgs args)
+        {
+            CicleLength = cicleTime.SelectedTime;
+        }
+
+        private void PauseTime_SelectedTimeChanged(TimePicker sender, TimePickerSelectedValueChangedEventArgs args)
+        {
+            PauseLength = pauseTime.SelectedTime;
+        }
+    }
+}
---
+++ TomatoTimer/TomatoTimer/Timer.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TomatoTimer
+{
+    /// <summary>
+    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// </summary>
+    public sealed partial class Timer : Page
+    {
+
+        public int CurCiclo { get; set; }
+        public int TotCicli { get; set; }
+        public double DurataCiclo { get; set; }
+        public double DurataPausa { get; set; }
+        private double InizioTimer { get; set; }
+
+        public Timer()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) 
+        {
+            AnotherPagePayload passedParameter = e.Parameter as AnotherPagePayload;
+            TotCicli = passedParameter.TotCicli;
+            DurataCiclo = passedParameter.DurataCiclo;
+            DurataPausa = passedParameter.DurataPausa;
+
+            CurCiclo = 1;
+            currentCicle.Text = CurCiclo.ToString();
+            totCicles.Text = TotCicli.ToString();
+            InizioTimer = DurataCiclo;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += Timer_Tick;
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            dispatcherTimer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            InizioTimer--;
+            TimeSpan time = TimeSpan.FromSeconds(InizioTimer);
+            string str = time.ToString(@"hh\:mm\:ss");
+            TimerLabel.Text = str;
+        }
+
+
+        // ToDelete
+        private void scrollText_Unloaded(object sender, RoutedEventArgs e)
+        {
+          //  timer.Stop();
+        }
+
+    }
+}
